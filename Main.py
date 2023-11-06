36b5ef28ff7319e2c3b32b89b3c03f554d64ab10
Praval Kollipara <pravalk19@gmail.com>
2023-11-06 12:02:03 -0500

    Slight changes to the code!

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 import os
+
 states_mapping = {
     'Alabama': 1,
     'Alaska': 2,
@@ -56,15 +57,14 @@ states_mapping = {
 
 def main():
     #train data
-    train = pd.read_csv("/Users/andyle/Desktop/Fall 2023 Classes/PHIL 208/states_training_data - states.csv")
+    train = pd.read_csv("states_training_data - states.csv")
     train = train.drop(columns='Abbreviation')
     label_mapping = {'Bad': 0, 'Decent': 1, 'Good': 2}
     train['Performance (Bad/Decent/Good)'] = train['Performance (Bad/Decent/Good)'].replace(label_mapping)
     train['State'] = train['State'].replace(states_mapping)
     train = train.replace(',','', regex=True)
-    #train['Population'] = train['Population'].astype(float)
-    #train['Land Area (sq mi)'] = train['Land Area (sq mi)'].astype(float)
     print(train)
+
     #make X_train
     X_train = train.drop('Performance (Bad/Decent/Good)', axis=1)
 
@@ -72,34 +72,31 @@ def main():
     y_train = train['Performance (Bad/Decent/Good)']
 
     #test data
-    test = pd.read_csv("/Users/andyle/Desktop/Fall 2023 Classes/PHIL 208/states_test_data - states.csv")
+    test = pd.read_csv("states_test_data - states.csv")
     test = test.drop(columns='Abbreviation')
     test['State'] = test['State'].replace(states_mapping)
     test = test.replace(',','', regex=True)
-    #test['Population'] = test['Population'].astype(float)
-    #test['Land Area (sq mi)'] = test['Land Area (sq mi)'].astype(float)
+
     X_test = test.drop('Performance (Bad/Decent/Good)', axis=1)
-    print(X_train)
-    print(y_train)
-    print(X_test)
     X_train_np = X_train.to_numpy()
-    print(X_train_np)
+    # print(X_train_np)
     y_train_np = y_train.to_numpy()
-    print(y_train_np)
+    # print(y_train_np)
     X_test_np = X_test.to_numpy()
     knn = KNeighborsClassifier(n_neighbors=1)
     knn.fit(X_train_np,y_train_np)
     y_pred = knn.predict(X_test_np)
-    print("Y_PRED")
-    print(y_pred)
-    print(len(y_pred))
-    test_new = pd.read_csv("/Users/andyle/Desktop/Fall 2023 Classes/PHIL 208/states_test_data - states.csv")
+    # print("Y_PRED")
+    # print(y_pred)
+    # print(len(y_pred))
+    test_new = pd.read_csv("states_test_data - states.csv")
     test_new['Performance (Bad/Decent/Good)'] = y_pred
 
     reverse_mapping = {0: 'Bad', 1: 'Decent', 2: 'Good'}
     test_new['Performance (Bad/Decent/Good)'] = test_new['Performance (Bad/Decent/Good)'].replace(reverse_mapping)
-    test_new.to_csv("/Users/andyle/Desktop/Fall 2023 Classes/PHIL 208/predictions.csv")
-    print(test_new)
+    test_new.to_csv("")
+    # print(test_new)
 
-    if __name__ == '__main__':
-        main()
+
+if __name__ == '__main__':
+    main()
